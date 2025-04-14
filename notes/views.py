1658a974aa4fc0f4d41721255b207eff52a9fa1a
SlavikPanasiuk <parapanasiuk.slvk@gmail.com>
2025-04-14 19:36:01 +0300

    homework40

--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-
+from .models import Note
 
 def notes_list(request):
-    test_notes = [
-        "Buy groceries",
-        "Call Alice",
-        "Read Django docs",
-        "Walk the dog"
-    ]
-    return render(request, 'notes/index.html', {'notes': test_notes})
+    notes = Note.objects.select_related('category').all()
+    return render(request, 'notes/index.html', {'notes': notes})
 
 
