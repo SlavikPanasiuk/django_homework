d615766652781613c4a8c4eff0fd6e3ef41b69f4
SlavikPanasiuk <parapanasiuk.slvk@gmail.com>
2025-04-26 10:44:18 +0300

    homework41

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns =[
     path('', views.notes_list, name='notes_list'),
+    path('notes/create/', views.create_note, name='create_note'),
+    path('notes/<int:pk>/', views.note_detail, name='note_detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Note
+from .forms import NoteForm
 
 def notes_list(request):
     notes = Note.objects.select_related('category').all()
     return render(request, 'notes/index.html', {'notes': notes})
 
+def create_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'create_note.html', {'form': form})
 
+def note_detail(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            form = NoteForm(request.POST, instance=note)
+            if form.is_valid():
+                form.save()
+                return redirect('note_detail', pk=note.pk)
+        elif 'delete' in request.POST:
+            note.delete()
+            return redirect('note_list')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'note_detail.html', {'form': form, 'note': note})
